38d322431f6da87fa5885a0c6ae4854a852cf916
KodexDigital <kodexkenth@gmail.com>
2025-07-08 16:43:04 +0100

    updated and ready to deploy after few data change

--- Steganography/wwwroot/service-worker.js
+++ Steganography/wwwroot/service-worker.js
@@ -1,19 +1,7 @@
-﻿const CACHE_NAME = "stego-cache-v1";
-const OFFLINE_URL = "/offline.html";
-
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(["/", "/manifest.json", "/css/site.css", OFFLINE_URL]);
-        })
-    );
-    self.skipWaiting();
+﻿self.addEventListener('install', function (e) {
+    console.log('Service Worker: Installed');
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request).then(
-            (response) => response || caches.match(OFFLINE_URL)
-        ))
-    );
+self.addEventListener('fetch', function (event) {
+    // Can add caching logic here
 });
