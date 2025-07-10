9d70a4f35330c98a3a8799d48d8602d78d9674d8
KodexDigital <kodexkenth@gmail.com>
2025-07-10 17:23:51 +0100

    updated

--- Steganography/Controllers/HomeController.cs
+++ Steganography/Controllers/HomeController.cs
@@ -8,15 +8,10 @@ namespace Steganography.Controllers
     {
         private readonly ILogger<HomeController> _logger = logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
+        public IActionResult Privacy() => View();
+        public IActionResult Cookies() => View();
+        public IActionResult Terms() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
